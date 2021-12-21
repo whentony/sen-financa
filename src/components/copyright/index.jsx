388ebc6e9c4dbc839b021321eae3741c4e55fdb4
Whentony Ferreira <whentony.ferreira@gmail.com>
2021-12-21 20:53:49 -0300

    component copyright

---
+++ src/components/copyright/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../../assets/css/copyright.module.css'
+const Copyright = () => {
+    return(
+        <div className={styles.containerCopyright}>
+           <a href='https://whentonyferreira.dev'>Developer Whentony Ferreira</a> 
+        </div>
+    )
+}
+
+export default Copyright;
