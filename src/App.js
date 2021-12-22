e7b878757d81b1d14a9f517d1e1d8819fb340399
Whentony Ferreira <whentony.ferreira@gmail.com>
2021-12-21 21:05:54 -0300

    component bar-top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Copyright from './components/copyright';
+import Bartop from './components/bar-top';
 
 function App() {
   return (
     <div>
+      <Bartop />
+      <div>teste</div>
       <Copyright />
     </div>
   );
---
+++ src/components/bar-top/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../../assets/css/bar-top.module.css'; 
+
+const Bartop = () => {
+    return(
+        <div className={styles.containerBartop}>whentony.ferreira@gmail.com</div>
+    )
+}
+
+export default Bartop;
